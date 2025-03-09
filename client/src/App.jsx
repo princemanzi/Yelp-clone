52558b0ff22632221dba83621b53e56074c3286e
Prince Manzi <manziprincemp@gmail.com>
2025-03-09 20:08:18 +0200

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UpdatePage from "./routes/UpdatePage";
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 
 const App = () => {
-    return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+        <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestaurantdetailPage = () => {
+  return (
+    <div>Detail Page</div>
+  )
+}
+
+export default RestaurantdetailPage
