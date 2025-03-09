f5047d2161c39e543206d89890987bd0806d3f23
Prince Manzi <manziprincemp@gmail.com>
2025-03-09 20:42:29 +0200

    initial commit

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
+import AddRestaurants from '../components/AddRestaurants'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <AddRestaurants/>
+    </div>
   )
 }
 
